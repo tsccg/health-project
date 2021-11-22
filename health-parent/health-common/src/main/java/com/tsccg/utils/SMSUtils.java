9c01edbbe23b156390d60ff786eae882d7f6d97b
TSCCG <2387600954@qq.com>
2021-11-22 14:15:04 +0800

    mobile commit

---
+++ health-parent/health-common/src/main/java/com/tsccg/utils/SMSUtils.java
@@ -0,0 +1,8 @@
+package com.tsccg.utils;
+
+/**
+ * @Author: TSCCG
+ * @Date: 2021/11/21 16:11
+ */
+public class SMSUtils {
+}
---
+++ health-parent/health-mobile/src/main/java/com/tsccg/controller/SetmealController.java
@@ -0,0 +1,8 @@
+package com.tsccg.controller;
+
+/**
+ * @Author: TSCCG
+ * @Date: 2021/11/21 16:54
+ */
+public class SetmealController {
+}
